cf1c20de27f54e8339e510dabdec429acc7a90b3
joelnyongesa <joelnyongesa148@gmail.com>
2024-01-26 15:07:55 +0300

    feat: ran the migrations inside app.py, creating the database and tables

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_migrate import Migrate
+
+from config import ApplicationConfig
+
+from models import db, User, Agent, Message
 
 app = Flask(__name__)
+
+app.config.from_object(ApplicationConfig)
+
+migrate = Migrate(app=app, db=db)
+
+db.init_app(app=app)
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
